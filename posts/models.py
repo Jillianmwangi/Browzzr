8ed7c30c39cb0d81ee5aea5acad9cd9c07c74745
Jillian Mwangi <mwangyjill@gmail.com>
2024-10-17 18:24:29 +0300

    settings.py modification

--- posts/models.py
+++ posts/models.py
@@ -25,14 +25,6 @@ class Comment(models.Model):
         return f'Comment by {self.author} on {self.post}'
 
 
-
-
-# class Post(models.Model):
-#     title = models.CharField(max_length=255)
-#     content = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
 class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
